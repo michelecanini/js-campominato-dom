7b59de6c542cdf73893517aa368c097575709f8e
michelecanini <studio.michelecanini@gmail.com>
2023-05-29 18:25:38 +0200

    [UPLOAD] insert update file

--- js/script.js
+++ js/script.js
@@ -2,25 +2,44 @@
 //PROBLEMA: L'utente clicca su un bottone che genererà una griglia di gioco quadrata. Ogni cella ha un numero progressivo, da 1 a 100. Ci saranno quindi 10 caselle per ognuna delle 10 righe. Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 
 // FUNZIONE PER CREARE LA SINGOLA CELLA
-function create_cell() {
+function createCell() {
     let cell = document.createElement('div');
     cell.classList.add('cell');
     return cell;
 }
 
-// COSTANTE PER IL RECUPERO DEL CONTENITORE HTML
-const grid = document.getElementById('grid');
+// RECUPERO BOTTONE PLAY CHE GENERA LA GRIGLIA QUADRATA
+const btn = document.getElementById('btn');
 
-// CICLO FOR PER CICLARE LE 100 CELLE
-for (let i = 0; i < 100; i++) {
-    let cell = create_cell();
-    cell.innerHTML = i + 1;
+btn.addEventListener('click', function(){
+
+    // COSTANTE PER IL RECUPERO DEL CONTENITORE HTML
+    const grid = document.getElementById('grid');
+
+    // RESET DELLE CELLE
+    grid.innerHTML = '';
+
+    // COSTANTE DEL NUMERO MASSIMO DI CELLE
+    const cellMax = 100;
+
+    // CICLO FOR PER CICLARE LE 100 CELLE
+    for (let i = 1; i <= cellMax; i++) {
+    let cell = createCell();
+    cell.innerHTML = i;
 
     cell.addEventListener('click', function(){
-        console.log(this);
-            
+        
+        this.classList.add('clicked_cell');
+            console.log(this.innerHTML);
         });
 
         grid.append(cell);
-        console.log(i);
 }
+});
+
+
+
+
+
+
+
