8ce7faa8636532db7ea6c8e1d620c514f44fb338
michelecanini <studio.michelecanini@gmail.com>
2023-05-29 22:52:59 +0200

    [UPDATE] insert function generate random number

--- js/script.js
+++ js/script.js
@@ -7,9 +7,15 @@ function createCell() {
     return cell;
 }
 
+// FUNZIONE PER LA CREAZIONE DEL NUMERO CASUALE
+function generateRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 // RECUPERO BOTTONE PLAY CHE GENERA LA GRIGLIA QUADRATA
 const btn = document.getElementById('btn');
 
+// FUNZIONE DEL BOTTONE PLAY DI AVVIO DEL GIOCO
 btn.addEventListener('click', function(){
 
     // COSTANTE PER IL RECUPERO DEL CONTENITORE HTML
@@ -20,6 +26,8 @@ btn.addEventListener('click', function(){
 
     // COSTANTE DEL NUMERO MASSIMO DI CELLE
     const cellMax = 100;
+
+    console.log(generateRandomNumber(1, cellMax));
 
     // CICLO FOR PER CICLARE LE 100 CELLE
     for (let i = 1; i <= cellMax; i++) {
