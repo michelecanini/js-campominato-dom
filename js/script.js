23f03ae796202019ef4502cd7adf8569c652f6ac
michelecanini <studio.michelecanini@gmail.com>
2023-06-02 18:47:31 +0200

    [UPDATE] insert array explosive tnt

--- js/script.js
+++ js/script.js
@@ -3,29 +3,41 @@
 //PROBLEMA: Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina. Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe). Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
 
 
-
-
-
 // LOGICA DEL PROGRAMMA - CREAZIONE DELLE FUNZIONI
 
-// FUNZIONE 0 - creazione di una nuava partita
-function newGame() {
-    const grid = document.getElementById('grid'); // costante per il recupero del contenitore html
-
-    // livello di difficoltà  
-    const difficulty_level = parseInt(document.getElementById('difficulty_level').value); 
-
-    grid.innerHTML = ''; // reset delle celle
-
-    createCell(difficulty_level); // fuzione che crea le celle del gioco
+// FUNZIONE 0 - generazione del numero casuale
+function generateRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// FUNZIONE 1 - creazione della singola cella
-function createCell(level) {
-   console.log(level)
+// FUNZIONE 1 - riempimento dell'arrey con esplosivo tnt
+function fillExplosiveTnt(array_explosive_tnt, max) {
+
+    // variabile di controllo
+    let control = false;
+    let random;
+
+    while (control === false) {
+        random = generateRandomNumber(1, max);
+        
+        if (!array_explosive_tnt.includes[random]) {
+            control = true;
+        }
+    }
+    return random;
+}
+
+// FUNZIONE 2 - creazione di una nuava partita
+function newGame() {
+    const grid = document.getElementById('grid'); // costante per il recupero del contenitore html
+    const array_explosive_tnt = []; // costante dell'array esplosivi tnt vuoto
+
+    // costante di recupero dei valori del livello di difficoltà  
+    const difficulty_level = parseInt(document.getElementById('difficulty_level').value); 
+
     // livelli di difficoltà
     let cellNumber;
-    switch (level) {
+    switch (difficulty_level) {
         case 0:
         cellNumber = 100;
         break;
@@ -36,34 +48,43 @@ function createCell(level) {
         cellNumber = 49;
         break;
     }
-    console.log(cellNumber);
 
-    // ciclo for per ciclare le 100 celle
-    for (let i = 0; i < cellNumber; i++) {
-    const cell = document.createElement('div');
+    for (let i = 0; i < 16; i++) {
+        let number = fillExplosiveTnt(array_explosive_tnt, cellNumber); // fuzione che riepie l'array con l'esplosivo tnt
+        array_explosive_tnt.push(number);
+    }
+    console.log(array_explosive_tnt)
 
-    let cellForRow = Math.sqrt(cellNumber);
+    grid.innerHTML = ''; // reset delle celle
 
-    cell.classList.add('cell');
-    cell.style.width =`calc(100% / ${cellForRow})`;
-    cell.style.height = cell.style.width;
-    cell.innerHTML = i + 1;
+    createCell(cellNumber); // fuzione che crea le celle del gioco
+}
 
-    cell.addEventListener('click', function(){
-     
-    this.classList.add('clicked_cell');
-         console.log(this.innerHTML);
- })
+// FUNZIONE 3 - creazione della singola cella
+function createCell(cellx) {
 
-     grid.append(cell);
-
-}};
-
-const array_explosive_tnt = []; // costante dell'array esplosivi tnt vuoto
-
-// FUNZIONE 2 - riempimento dell'arrey con esplosivo tnt
-//function fillExplosiveTnt(array_explosive_tnt, max) {
     
+    // ciclo for per ciclare le 100 celle
+    for (let i = 0; i < cellx; i++) {
+        const cell = document.createElement('div');
+
+        let cellForRow = Math.sqrt(cellx);
+
+        cell.classList.add('cell');
+        cell.style.width =`calc(100% / ${cellForRow})`;
+        cell.style.height = cell.style.width;
+
+        cell.innerHTML = i + 1;
+
+        cell.addEventListener('click', function(){
+            this.classList.add('clicked_cell');
+            console.log(this.innerHTML);
+        })
+
+        grid.append(cell);
+    }
+}
+
 //    let control = false;
 //    let random_number;
 
@@ -79,10 +100,6 @@ const array_explosive_tnt = []; // costante dell'array esplosivi tnt vuoto
 //    return random_number;
 //}
 
-// FUNZIONE 3 - generazione del numero casuale
-//function generateRandomNumber(min, max) {
-//    return Math.floor(Math.random() * (max - min + 1)) + min;
-//}
 
 // ciclo for di chiamata della funzione per riempire l'array degli esplosivi tnt
 //for (let i=0; i<16; i++) {
