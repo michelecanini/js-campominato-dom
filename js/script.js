6cb65694e68bb7a9c0be771a7b417b73dcb5d22d
michelecanini <studio.michelecanini@gmail.com>
2023-06-02 10:56:50 +0200

    [UPLOAD] correct comment

--- js/script.js
+++ js/script.js
@@ -1,22 +1,24 @@
 "use strict";
 
+//PROBLEMA: Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina. Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe). Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
+
+
 // DICHIARAZIONE DELLE VARIABILI
 
 const array_explosive_tnt = []; // costante dell'array esplosivi tnt vuoto
 const grid = document.getElementById('grid'); // costante per il recupero del contenitore html
 const btn = document.getElementById('btn'); // recupero bottone play che genera la griglia
 
-
 // LOGICA DEL PROGRAMMA - CREAZIONE DELLE FUNZIONI
 
-// funzione 1 - creazione della singola cella
+// FUNZIONE 1 - creazione della singola cella
 function createCell() {
     let cell = document.createElement('div');
     cell.classList.add('cell');
     return cell;
 }
 
-// funzione 2 - riempimento dell'arrey con esplosivo tnt
+// FUNZIONE 2 - riempimento dell'arrey con esplosivo tnt
 function fillExplosiveTnt(array_explosive_tnt, max) {
     
     let control = false;
@@ -34,32 +36,30 @@ function fillExplosiveTnt(array_explosive_tnt, max) {
     return random_number;
 }
 
-// funzione 3 - generazione del numero casuale
+// FUNZIONE 3 - generazione del numero casuale
 function generateRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
- // CHIAMATA DELLA FUNZIONE PER RIEMPIRE L'ARRAY DEGLI ESPLOSIVI TNT
- for (let i=0; i<16; i++) {
-    let number = fillExplosiveTnt(array_explosive_tnt);
-array_explosive_tnt.push(number);
+// ciclo for di chiamata della funzione per riempire l'array degli esplosivi tnt
+//for (let i=0; i<16; i++) {
+//    let number = fillExplosiveTnt(array_explosive_tnt);
+//array_explosive_tnt.push(number);
 
-console.log(array_explosive_tnt);
-}
+//console.log(array_explosive_tnt);
+//}
 
-
-
-// FUNZIONE DEL BOTTONE PLAY DI AVVIO DEL GIOCO
+// FUNZIONE 4 - bottone play di avvio del gioco
 btn.addEventListener('click', function(){
 
     // reset delle celle
     grid.innerHTML = '';
     
-    // COSTANTE DEL NUMERO MASSIMO DI CELLE
+    // constante del numero massimo di celle
     const cellMax = 100;
     console.log(generateRandomNumber(1, cellMax));
 
-    // CICLO FOR PER CICLARE LE 100 CELLE
+    // ciclo for per ciclare le 100 celle
     for (let i = 1; i <= cellMax; i++) {
     let cell = createCell();
     cell.innerHTML = i;
@@ -72,5 +72,4 @@ btn.addEventListener('click', function(){
 
         grid.append(cell);
     }
-});
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
+});                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
--- readme.md
+++ readme.md
@@ -1,18 +1,18 @@
 # JS-CAMPOMINATO-DOM
 
-### PROBLEMA: Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina. Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe). Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
+#### PROBLEMA: Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina. Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe). Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
 
-### Sotto-problema-1: Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+#### Sotto-problema-1: Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
     1. creare una funzione per la generazione di un numero randomico
     2. creare un array vuoto per la generazione di 16 bombe
     3. aggiungere una select (bottone) per 3 diversi livelli di difficoltà
 
-### Sotto-problema-2: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
+#### Sotto-problema-2: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
     1. creare un ciclo for o while
     2. inserire un controllo per escludere due numeri uguali nell'array
 
-### Sotto-problema-3: In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina. Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+#### Sotto-problema-3: In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina. Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
 
-### Sotto-problema-4: La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+#### Sotto-problema-4: La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
 
-### Sotto-problema-5: Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
+#### Sotto-problema-5: Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
