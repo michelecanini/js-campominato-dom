64f5350d0393c1da47623fab40fd060121dc2be6
michelecanini <studio.michelecanini@gmail.com>
2023-06-02 10:22:58 +0200

    [DOCS] update readme

--- js/script.js
+++ js/script.js
@@ -1,43 +1,62 @@
 "use strict";
 
-// FUNZIONE PER CREARE LA SINGOLA CELLA
+// DICHIARAZIONE DELLE VARIABILI
+
+const array_explosive_tnt = []; // costante dell'array esplosivi tnt vuoto
+const grid = document.getElementById('grid'); // costante per il recupero del contenitore html
+const btn = document.getElementById('btn'); // recupero bottone play che genera la griglia
+
+
+// LOGICA DEL PROGRAMMA - CREAZIONE DELLE FUNZIONI
+
+// funzione 1 - creazione della singola cella
 function createCell() {
     let cell = document.createElement('div');
     cell.classList.add('cell');
     return cell;
 }
 
-// COSTANTE DELL'ARRAY ESPLOSIVI TNT VUOTO
-const array_explosive_tnt = [];
+// funzione 2 - riempimento dell'arrey con esplosivo tnt
+function fillExplosiveTnt(array_explosive_tnt, max) {
+    
+    let control = false;
+    let random_number;
 
-// FUNZIONE DI RIEMPIMENTO DELL'ARREY ESPLOSIVO TNT
-function fillExplosiveTnt(array_explosive_tnt) {
-    console.log(array_explosive_tnt);
+    while (control === false) {
+
+        // creazione del numero random
+        random_number = generateRandomNumber(1, max);
+        
+        if (!array_explosive_tnt.includes(random_number)) {
+            control = true;
+        }
+}
+    return random_number;
 }
 
-// FUNZIONE PER LA CREAZIONE DEL NUMERO CASUALE
+// funzione 3 - generazione del numero casuale
 function generateRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// RECUPERO BOTTONE PLAY CHE GENERA LA GRIGLIA QUADRATA
-const btn = document.getElementById('btn');
+ // CHIAMATA DELLA FUNZIONE PER RIEMPIRE L'ARRAY DEGLI ESPLOSIVI TNT
+ for (let i=0; i<16; i++) {
+    let number = fillExplosiveTnt(array_explosive_tnt);
+array_explosive_tnt.push(number);
+
+console.log(array_explosive_tnt);
+}
+
+
 
 // FUNZIONE DEL BOTTONE PLAY DI AVVIO DEL GIOCO
 btn.addEventListener('click', function(){
 
-    // COSTANTE PER IL RECUPERO DEL CONTENITORE HTML
-    const grid = document.getElementById('grid');
-
-    // RESET DELLE CELLE
+    // reset delle celle
     grid.innerHTML = '';
-
-    // CHIAMATA DELLA FUNZIONE PER RIEMPIRE L'ARRAY DEGLI ESPLOSIVI TNT
-    fillExplosiveTnt(array_explosive_tnt);
-
+    
     // COSTANTE DEL NUMERO MASSIMO DI CELLE
     const cellMax = 100;
-
     console.log(generateRandomNumber(1, cellMax));
 
     // CICLO FOR PER CICLARE LE 100 CELLE
@@ -52,5 +71,6 @@ btn.addEventListener('click', function(){
         });
 
         grid.append(cell);
-}
+    }
 });
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
